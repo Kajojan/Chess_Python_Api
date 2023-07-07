45a72a94a273fc4dec7e7936a20d007efd5a7354
Kajojan <kojoty1999@gmail.com>
2023-07-07 13:47:42 +0200

    invalid value

--- board.py
+++ board.py
@@ -2,12 +2,15 @@ class Board:
     def __init__(self):
         self.board = [[" " for _ in range(8)] for _ in range(8)]
 
-    def place_piece(self, position , piece):
+    def place_piece(self, position, piece):
         col, row = position[0], int(position[1:])
         col = col.lower()
-        if len(col) == 1 and "a" <= col <= "h":
-            col = ord(col) - ord("a")
+        if (len(col) != 1 or col not in "abcdefgh") and (not (1 <= row <= 8)):
+            raise ValueError("Invalid column and row values")
+        elif len(col) != 1 or col not in "abcdefgh":
+            raise ValueError("Invalid column value")
+        elif not (1 <= row <= 8):
+            raise ValueError("Invalid row value")
         else:
-            raise ValueError("Invalid column value.")
-
+            col = ord(col) - ord("a")
         self.board[row - 1][col] = piece
