17e197b333768897fe1ee9c7d70766b800d202a8
Kajojan <kojoty1999@gmail.com>
2023-07-07 14:14:50 +0200

    api response tests

---
+++ test/api_test.py
@@ -0,0 +1,77 @@
+import requests
+import pytest
+
+
+@pytest.fixture
+def api_url():
+    return "http://localhost:8000/api/v1"
+
+
+def test_get_available_moves(api_url):
+    chess_figure = "rook"
+    current_field = "H2"
+    expected_response = {
+        "availableMoves": ["H3"],
+        "error": None,
+        "figure": "rook",
+        "currentField": "H2",
+    }
+
+    response = requests.get(f"{api_url}/{chess_figure}/{current_field}")
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
+
+def test_get_available_moves_valid_field(api_url):
+    chess_figure = "rook"
+    current_field = "H15"
+    expected_response = {
+        "availableMoves": [],
+        "error": "Field does not exist.",
+        "figure": "rook",
+        "currentField": "H15",
+    }
+
+    response = requests.get(f"{api_url}/{chess_figure}/{current_field}")
+    assert response.status_code == 409
+    assert response.json() == expected_response
+
+
+def test_not_existing_figure(api_url):
+    chess_figure = "rooook"
+    current_field = "H2"
+
+    response = requests.get(f"{api_url}/{chess_figure}/{current_field}")
+    assert response.status_code == 404
+
+
+def test_valid_move(api_url):
+    chess_figure = "rook"
+    current_field = "H2"
+    dest_field = "H3"
+    expected_response = {
+        "move": "valid",
+        "figure": "rook",
+        "error": None,
+        "currentField": "H2",
+        "destField": "H3",
+    }
+    response = requests.get(f"{api_url}/{chess_figure}/{current_field}/{dest_field}")
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
+
+def test_invalid_move(api_url):
+    chess_figure = "rook"
+    current_field = "H2"
+    dest_field = "A3"
+    expected_response = {
+        "move": "invalid",
+        "figure": "rook",
+        "error": "Current move is not permitted.",
+        "currentField": "H2",
+        "destField": "A3",
+    }
+    response = requests.get(f"{api_url}/{chess_figure}/{current_field}/{dest_field}")
+    assert response.status_code == 409
+    assert response.json() == expected_response
